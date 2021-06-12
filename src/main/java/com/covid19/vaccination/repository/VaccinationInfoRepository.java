8ccdae60ccfd1b8c2bc3626a4ae014f06e5bf022
ab68478 <ab68478@host.docker.internal>
2021-06-12 17:08:34 +0530

    Updated Code

--- src/main/java/com/covid19/vaccination/repository/VaccinationInfoRepository.java
+++ src/main/java/com/covid19/vaccination/repository/VaccinationInfoRepository.java
@@ -9,6 +9,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
@@ -31,11 +32,13 @@ public class VaccinationInfoRepository {
 		
 		vaccinationInfo.setDate(sf.format(new Date()));
 		mongoTemplate.save(vaccinationInfo);
-		System.out.println(vaccinationInfo.getDate());
+		
 	}
 
-	public VaccinationInfo getVaccinationInfo() {
+	public VaccinationInfo getVaccinationInfo(String districtId) {
+		Criteria criteria = new Criteria("district").is(districtId);
 		Query query = new Query();
+		query.addCriteria(criteria);
 		query.with(Sort.by(Sort.Direction.DESC,"_id"));
 		query.limit(1);
 		return mongoTemplate.findOne(query, VaccinationInfo.class);
