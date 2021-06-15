96d9bce75c0c62ce291653d704280bf3d908740c
ab68478 <ab68478@IND-5CG9381KGD.ctl.intranet>
2021-06-15 15:00:21 +0530

    Ip check Functionality

--- src/main/java/com/covid19/vaccination/controller/StateListController.java
+++ src/main/java/com/covid19/vaccination/controller/StateListController.java
@@ -3,6 +3,8 @@
  */
 package com.covid19.vaccination.controller;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.covid19.vaccination.model.State;
 import com.covid19.vaccination.service.StateService;
+import com.covid19.vaccination.vo.StatusMessageVo;
 
 /**
  * @author ab68478
@@ -30,6 +33,15 @@ public class StateListController {
 	public List<State> getStateList(){
 		List<State> states = stateService.getStates();
 		return states;
+	}
+	
+	@RequestMapping(value = "/ip")
+	public StatusMessageVo getServerIp() throws UnknownHostException{
+		StatusMessageVo vo = new StatusMessageVo();
+		String ipAddress = InetAddress.getLocalHost().getHostName();
+		vo.setStatusMessage("ipAddress");
+		
+		return vo;
 	}
 
 }
