e812c3faf9515862033c9bb3b76e79d8d5447ad3
ab68478 <ab68478@host.docker.internal>
2021-06-17 13:29:05 +0530

    Code Changes

---
+++ src/main/java/com/covid19/vaccination/controller/ServiceDiscoveryVo.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.covid19.vaccination.controller;
+
+/**
+ * @author ab68478
+ *
+ */
+public class ServiceDiscoveryVo {
+	private String url;
+
+	/**
+	 * @return the url
+	 */
+	public String getUrl() {
+		return url;
+	}
+
+	/**
+	 * @param url the url to set
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
