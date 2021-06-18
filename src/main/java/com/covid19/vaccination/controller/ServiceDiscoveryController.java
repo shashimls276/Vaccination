9e60d430eb32d27458fa8091484aa663af60680d
ab68478 <ab68478@host.docker.internal>
2021-06-18 14:07:34 +0530

    commit

--- src/main/java/com/covid19/vaccination/controller/ServiceDiscoveryController.java
+++ src/main/java/com/covid19/vaccination/controller/ServiceDiscoveryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,11 @@ public class ServiceDiscoveryController {
 		}
 		return serviceDiscoveryVo;
 		
+	}
+	
+	@GetMapping("/testing")
+	public String getString() {
+		return "Hello Latest Image";
 	}
 
 }
